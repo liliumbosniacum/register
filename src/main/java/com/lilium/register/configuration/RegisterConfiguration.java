fd876db03b2e9a672f261a4d01ca3ac6852af336
liliumbosniacum <71554170+liliumbosniacum@users.noreply.github.com>
2025-03-26 06:35:32 +0100

    Added elasticsearch configuration

---
+++ src/main/java/com/lilium/register/configuration/RegisterConfiguration.java
@@ -0,0 +1,37 @@
+package com.lilium.register.configuration;
+
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+import javax.net.ssl.SSLContext;
+
+@Configuration
+public class RegisterConfiguration extends ElasticsearchConfiguration {
+
+    @Value("${register.elasticsearch.user}")
+    private String username;
+
+    @Value("${register.elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedToLocalhost()
+                .usingSsl(builSSLContext())
+                .withBasicAuth(username, password)
+                .build();
+    }
+
+    private static SSLContext builSSLContext() {
+        try {
+            return new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
